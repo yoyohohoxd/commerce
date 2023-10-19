09b802f9c80746a8552a5e29c195205df1f41d6a
yoyohohoxd_WSL <madse@live.dk>
2023-10-19 10:54:53 +0200

    changed comment-layout

--- auctions/models.py
+++ auctions/models.py
@@ -44,3 +44,6 @@ class Comment(models.Model):
     comment = models.CharField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", default=1)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="discussions", default=1)
+
+    def __str__(self):
+        return f"{self.user} said: {self.comment}"
